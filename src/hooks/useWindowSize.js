7dfd4b72bf176b0a9cd4f644a94e47d36eda5ea8
Ivan Tusnolobov <ivan.tusnolobov@gmail.com>
2019-10-11 23:40:18 +0300

    Fix hook error that occured during deployment.. I guess

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,16 +1,22 @@
 import { useState, useEffect } from 'react';
 
 export const useWindowSize = () => {
+  const isClient = typeof window === 'object';
+
   const getSize = () => {
     return {
-      width: window.innerWidth,
-      heigth: window.innerHeight,
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
     };
   };
 
   const [windowSize, setWindowSize] = useState(getSize());
 
   useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
     const handleResize = () => {
       setWindowSize(getSize());
     };
