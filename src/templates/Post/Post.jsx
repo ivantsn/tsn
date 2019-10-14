452ce74a8308fc241cbc39f35da949f3e63ec407
Ivan Tusnolobov <ivan.tusnolobov@gmail.com>
2019-10-15 01:11:51 +0300

    Add a function that generates pages

---
+++ src/templates/Post/Post.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { css } from '@emotion/core';
+
+import { formatDate } from '../../utils/helpers';
+import { Layout } from '../../components/Layout/Layout';
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      timeToRead
+      body
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => {
+  return (
+    <Layout>
+      <article
+        css={theme => css`
+          grid-column: 1/5;
+
+          @media (${theme.breakPoints.medium.query}) {
+            grid-column: 2/4;
+          }
+        `}
+      >
+        <h1>{post.frontmatter.title}</h1>
+        <p>
+          {formatDate(post.frontmatter.date)} | {post.timeToRead} mins to read
+        </p>
+        <section>
+          <MDXRenderer>{post.body}</MDXRenderer>
+        </section>
+      </article>
+    </Layout>
+  );
+};
+export default PostTemplate;
