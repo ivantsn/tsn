1acc476bdf6e6ca50156c13d61748c29783f4520
Ivan Tusnolobov <ivan.tusnolobov@gmail.com>
2019-10-14 19:55:54 +0300

    Change post preview basic structure

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { css } from '@emotion/core';
+import { Link } from 'gatsby';
 
-export const PostPreview = ({ title, description, timeToRead, slug }) => {
+export const PostPreview = ({
+  post: { title, timeToRead, description, slug },
+}) => {
   return (
-    <section>
-      <h2>{title}</h2>
-      <p>{timeToRead}</p>
+    <article
+      css={theme => css`
+        grid-column: 1/5;
+
+        @media (${theme.breakPoints.medium.query}) {
+          grid-column: 2/4;
+        }
+      `}
+    >
+      <Link to={`blog/${slug}`}>
+        <h2>{title}</h2>
+      </Link>
+      <p>{timeToRead} mins to read</p>
       <p>{description}</p>
-    </section>
+    </article>
   );
 };
